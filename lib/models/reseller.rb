c03b87606b8ec551ae4939913df315e5b0294953
Alexey Pisarenko <alp@itcrimea.com>
2015-02-05 14:14:30 +0300

    Fixing resseler

--- lib/models/reseller.rb
+++ lib/models/reseller.rb
@@ -1,21 +1,22 @@
 require_relative '../mixin.rb'
 
-class Reseller
-  class << self
-    BASE_URL = "https://test.httpapi.com/api/customers/"
+module ResellerClub
+  class Reseller
+    class << self
+      BASE_URL = "https://test.httpapi.com/api/resellers/"
 
-    extend ResellerClub
+      extend ResellerClub
 
-    [{"values" => {"lang_pref" => "en"}, "http_method" => "post","validate" => lambda {|v| true}, "url" => "signup.json"},
-     {"values" => {"lang_pref" => "en"}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "modify-details.json"},
-     {"values" => {"reseller_id" => ""}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "details.json"},
-     {"values" => {"reseller_id" => ""}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "temp-password.json"},
-     {"values" => {"no_of_records" => "50","page_no" => "1"}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "search.json"},
-     {"values" => {"customer_id" => ""}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "delete.json"},
-     {"values" => {},"http_method" => "get", "validate" => lambda {|v| true}, "url" => "generate-token.json"},
-     {"values" => {}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "authenticate-token.json"},
-     {"values" => {}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "promo-details.json"},
-    ].each { |p| build_method p }
+      [{"values" => {"lang_pref" => "en"}, "http_method" => "post","validate" => lambda {|v| true}, "url" => "signup.json"},
+       {"values" => {"lang_pref" => "en"}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "modify-details.json"},
+       {"values" => {"reseller_id" => ""}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "details.json"},
+       {"values" => {"reseller_id" => ""}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "temp-password.json"},
+       {"values" => {"no_of_records" => "50","page_no" => "1"}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "search.json"},
+       {"values" => {"customer_id" => ""}, "http_method" => "post", "validate" => lambda {|v| true}, "url" => "delete.json"},
+       {"values" => {},"http_method" => "get", "validate" => lambda {|v| true}, "url" => "generate-token.json"},
+       {"values" => {}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "authenticate-token.json"},
+       {"values" => {}, "http_method" => "get", "validate" => lambda {|v| true}, "url" => "promo-details.json"},
+      ].each { |p| build_method p }
+    end
   end
-
 end
