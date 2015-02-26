d71509fe592b6fe8b6a070dba0d90ce40ca3a178
Alexey Pisarenko <alp@itcrimea.com>
2015-02-26 18:13:11 +0300

    Adding bind to billing

--- lib/resellerclub.rb
+++ lib/resellerclub.rb
@@ -1,4 +1,5 @@
 require_relative "./mixin.rb"
+require_relative "./models/billing.rb"
 require_relative "./models/customer.rb"
 require_relative "./models/reseller.rb"
 require_relative "./models/contact.rb"
